7b0253e0e69a2b98f3d5e45eb2dc27143ab4d466
haliphax <haliphax@gmail.com>
2023-12-21 15:22:06 -0600

    ✨ locale selection, defaults to browser locale

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -6,10 +6,16 @@ const user = await fetch("https://api.twitch.tv/helix/users", { headers })
 	.then((r) => r.json())
 	.then((j) => j.data[0]);
 const form = document.querySelector("form")!;
-const voice = (document.getElementById("voice")! as HTMLInputElement).value;
+const localeInput = document.getElementById("locale")! as HTMLInputElement;
 
-form.action +=
-	`#oauth=${hs.access_token}` +
-	`&channel=${user.login}` +
-	`&voice=${voice}` +
-	"&reward=change-me";
+localeInput.value = window.navigator.language;
+
+form.addEventListener("submit", () => {
+	const voice = (document.getElementById("voice")! as HTMLInputElement).value;
+
+	form.action +=
+		`#oauth=${hs.access_token}` +
+		`&channel=${user.login}` +
+		`&voice=${localeInput.value}-${voice}` +
+		"&reward=change-me";
+});
